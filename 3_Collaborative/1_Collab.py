4de0cce9b568bba72d9ea460e5c7e25fc320879c
lintangwisesa <lintangwisesa@ymail.com>
2019-07-11 11:52:57 +0700

    recommender sys

---
+++ 3_Collaborative/1_Collab.py
@@ -0,0 +1,33 @@
+# Collaborative Filtering
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_excel('dataTV.xlsx', index_col=0)
+df.fillna(0, inplace=True)
+print(df)
+
+# ================================
+# correlation
+
+dfCorr = df.corr(method='pearson')
+print(dfCorr)
+# pearson : standard correlation coefficient
+# kendall : Kendall Tau correlation coefficient
+# spearman : Spearman rank correlation
+
+# ==============================
+# testing
+
+saya = [
+    ('sinetron_C', 5), ('sinetron_A', 4)
+]
+
+skorSama = pd.DataFrame()
+for produk, rating in saya:
+    skor = dfCorr[produk] * (rating - 2.5)
+    skor = skor.sort_values(ascending = False)
+    skorSama = skorSama.append(skor, ignore_index=False)
+
+print(skorSama)
+print(skorSama.sum().sort_values(ascending=False))
